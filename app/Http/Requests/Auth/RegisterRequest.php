f202575891d9e01888e386c757ec3f454b21d416
krit.pra <krit.prasert1983@gmail.com>
2024-12-16 10:37:00 +0700

    fix: register check unique email

--- app/Http/Requests/Auth/RegisterRequest.php
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -24,7 +24,7 @@ class RegisterRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
             'password' => ['required', 'string', 'min:8'],
             'password_confirm' => ['required', 'string', 'same:password'],
         ];
